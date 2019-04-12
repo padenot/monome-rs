5390d94c1c6883033dae7e8a72342a0d4f547dea
Paul Adenot <paul@paul.cx>
2019-04-12 16:51:15 +0200

    example for device notification

---
+++ examples/device_notification.rs
@@ -0,0 +1,18 @@
+extern crate env_logger;
+extern crate monome;
+
+use std::io;
+
+use monome::DeviceChangeNotifier;
+
+fn main() {
+    env_logger::init();
+
+    DeviceChangeNotifier::new(|event| {
+        println!("{:?}", event);
+    });
+    let mut answer = String::new();
+        io::stdin().read_line(&mut answer)
+            .ok()
+            .expect("Failed to read line");
+}
