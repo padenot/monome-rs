5f2a8486d512fec0b7717dacac928855c9e1909a
Paul Adenot <paul@paul.cx>
2019-06-17 10:45:25 -0700

    clean up arc example

--- examples/arc.rs
+++ examples/arc.rs
@@ -11,9 +11,7 @@ fn main() {
 
     println!("{:?}", monome);
 
-    let mut led: [f32; 4]= [0., 0., 0., 0.];
-    let mut ii: i32 = 0;
-    let mut dii: i32 = 1;
+    let mut led = [0.; 4];
 
     for i in 0..4 {
         monome.ring_all(i, 0);
@@ -21,39 +19,16 @@ fn main() {
 
     loop {
         let e = monome.poll();
-        ii+=dii;
-        if ii == 120 || ii == 0 {
-            dii = -dii;
+
+        for i in 0..4 {
+            monome.ring_set(i, led[i] as u32, 15);
         }
-
-         monome.ring_range(0, 0, 32, 15);
-         monome.ring_range(1, 32, 64, 15);
-         monome.ring_range(2, 16, 48, 15);
-         monome.ring_range(3, 48, 16, 15);
-        //let mut v: [u8; 64] = [0; 64];
-
-        //for i in 0..64 {
-        //    v[i] = (i / 4) as u8;
-        //}
-
-        //monome.ring_map(0, &v);
-        //monome.ring_map(1, &v);
-        //monome.ring_map(2, &v);
-        //monome.ring_map(3, &v);
-
-        // for i in 0..4 {
-        //     monome.ring_all(i, (ii / 10) as u32);
-        //     monome.ring_set(i, led[i] as u32, 15);
-        // }
         match e {
             Some(MonomeEvent::EncoderDelta {n, delta} ) => {
                 let n = n as usize;
                 monome.ring_set(n, led[n] as u32, 0);
                 led[n] = led[n] + (delta as f32 / 4.);
-               // if led[n] > 64. {
-               //     led[n] -= 64.;
-               // } else 
-                    if led[n] < 0. {
+                if led[n] < 0. {
                     led[n] += 64.;
                 }
                 monome.ring_set(n, led[n] as u32, 15);
