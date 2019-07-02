04238a34a71e316b3d7d0fa4c0a927ef3d9dcc5a
Paul Adenot <paul@paul.cx>
2019-07-02 08:26:32 +0200

    fix arc example to not spam the device

--- examples/arc.rs
+++ examples/arc.rs
@@ -15,25 +15,27 @@ fn main() {
 
     for i in 0..4 {
         monome.ring_all(i, 0);
+        monome.ring_set(i, led[i] as u32, 15);
     }
 
     loop {
-        let e = monome.poll();
+        loop {
+            let e = monome.poll();
 
-        for i in 0..4 {
-            monome.ring_set(i, led[i] as u32, 15);
-        }
-        match e {
-            Some(MonomeEvent::EncoderDelta { n, delta }) => {
-                let n = n as usize;
-                monome.ring_set(n, led[n] as u32, 0);
-                led[n] = led[n] + (delta as f32 / 4.);
-                if led[n] < 0. {
-                    led[n] += 64.;
+            match e {
+                Some(MonomeEvent::EncoderDelta { n, delta }) => {
+                    let n = n as usize;
+                    monome.ring_set(n, led[n] as u32, 0);
+                    led[n] = led[n] + (delta as f32 / 4.);
+                    if led[n] < 0. {
+                        led[n] += 64.;
+                    }
+                    monome.ring_set(n, led[n] as u32, 15);
                 }
-                monome.ring_set(n, led[n] as u32, 15);
+                _ => {
+                    break;
+                }
             }
-            _ => {}
         }
 
         let refresh = time::Duration::from_millis(10);
